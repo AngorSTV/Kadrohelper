a3e5885fb4a370abbae5c3d2effa9b8cdd1aa559
AngorSTV <angor@inbox.ru>
2016-07-04 16:23:09 +0300

    правильная мавенизация проекта со сборкой exe файла

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Андрей on 04.07.2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
