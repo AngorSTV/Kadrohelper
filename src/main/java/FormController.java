0febba88d5205099b0762cb6ba296d1d9e9cc0d3
AngorSTV <angor@inbox.ru>
2016-07-04 18:12:36 +0300

    вывод формы на экран

---
+++ src/main/java/FormController.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Андрей on 04.07.2016.
+ */
+public class FormController {
+    private Main mainClass;
+
+    public void setMainClass(Main mainClass) {
+        this.mainClass = mainClass;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,12 @@
  */
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -12,7 +17,15 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("form.fxml"));
+            Scene scene = new Scene(root);
+            stage.setTitle("Помошник кадровика");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
