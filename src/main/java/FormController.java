0f50f14903ec5acb4de1e22eb851950ef5e87ef7
AngorSTV <angor@inbox.ru>
2016-07-05 13:37:23 +0300

    считает период

--- src/main/java/FormController.java
+++ src/main/java/FormController.java
@@ -1,10 +1,56 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
 /**
  * Created by Андрей on 04.07.2016.
  */
 public class FormController {
-    private Main mainClass;
+    @FXML
+    private DatePicker startDatePicker;
+    @FXML
+    private DatePicker endDatePicker;
+    @FXML
+    private TextField result;
 
-    public void setMainClass(Main mainClass) {
-        this.mainClass = mainClass;
+    @FXML
+    private void initialize() {
+        result.setEditable(false);
+        endDatePicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                calculate();
+            }
+        });
+        startDatePicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                calculate();
+            }
+        });
+    }
+
+    private void calculate() {
+        if(startDatePicker.getValue() == null) {
+            result.setText("");
+            return;
+        }
+        if(endDatePicker.getValue() == null) {
+            result.setText("");
+            return;
+        }
+
+        LocalDate startDate = startDatePicker.getValue();
+        LocalDate endDate = endDatePicker.getValue();
+        Period period = Period.between(startDate, endDate) ;
+        String str = period.getYears() +" лет, " + period.getMonths() + " месяцев, ";
+        str += period.getDays() + " дней.";
+        result.setText(str);
     }
 }
